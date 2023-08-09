b25fcb418d2187e5aa2f79a18d107b36ed91bf94
Neeraj Kumar Tiwari <n.tiwari@celonis.com>
2023-08-09 12:47:28 +0200

    feat-001-nkt updated main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import structlog
 
 app = Flask(__name__)
@@ -10,7 +10,7 @@ def webhook():
     LOGGER.info("request received")
     event = request.get_data()
     LOGGER.info(f"ArgoCD Notification received is:  {event}")
-
+    return jsonify(event)
 
 if __name__ == "__main__":
     app.run(port=5000)
